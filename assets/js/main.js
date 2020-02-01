7cb5324302afd8e24ecf5a3ac77da8d00d0f12e1
Joseph Moberly <jmobes15@gmail.com>
2020-01-31 22:35:36 -0800

    wrote functions to prevent bugs when clicking on the same card twice or on different cards fast

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
   let firstCard = null;
   let secondCard = null;
   let flipped = false;
+  let locked = false;
   let match = 0;
 
   const cards = document.querySelectorAll(".card");
@@ -11,31 +12,59 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   function flipCard() {
+    let back = this.children[0];
+    let front = this.children[1];
 
-    console.log("Event Fired");
+    if (locked) {
+      return;
+    }
 
-    const back = this.children[0];
-    const front = this.children[1];
+    if(this === firstCard) {
+      return;
+    }
 
     back.classList.toggle("invisible");
     front.classList.toggle("visible");
 
     if(!flipped) {
       flipped = true;
-      firstCard = front;
-      console.log(this);
-    }
-    else {
-      flipped = false;
-      secondCard = front;
-      console.log(this);
-
-      if (firstCard.getAttributeNode("src").value === secondCard.getAttributeNode("src").value) {
-        console.log("ITS A MATCH");
-        match++;
-      }
+      firstCard = this;
+      return;
     }
 
+    flipped = false;
+    secondCard = this;
+    checkMatch();
+  }
+
+  function checkMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    isMatch? removeClick() : unflip();
+  }
+
+  function removeClick() {
+    console.log("ITS A MATCH");
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+  }
+
+  function unflip() {
+    locked = true;
+    setTimeout(() => {
+      firstCard.children[0].classList.toggle("invisible");
+      firstCard.children[1].classList.toggle("visible");
+      secondCard.children[0].classList.toggle("invisible");
+      secondCard.children[1].classList.toggle("visible");
+
+      resetBoard();
+    }, 1500);
+  }
+
+  function resetBoard() {
+    flipped = false;
+    locked = false;
+    firstCard = null;
+    secondCard = null;
   }
 
 });
